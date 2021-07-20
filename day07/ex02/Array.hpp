af4f2d2e6183a65356ef28ff165fc5ffceea9679
tgrangeo <thomas.grangeon9@gmail.com>
2021-07-20 11:11:14 +0200

    hi

--- day07/ex02/Array.hpp
+++ day07/ex02/Array.hpp
@@ -3,29 +3,77 @@
 /*                                                        :::      ::::::::   */
 /*   Array.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tgrangeo <tgrangeo@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: thomasgrangeon <thomasgrangeon@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/19 15:01:18 by tgrangeo          #+#    #+#             */
-/*   Updated: 2021/07/19 15:06:07 by tgrangeo         ###   ########lyon.fr   */
+/*   Updated: 2021/07/20 11:10:00 by thomasgrang      ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef _ARRAY_HPP_
-# define _ARRAY_HPP_
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
 
-template <typename T>
-class Array{
+#include <stdexcept>
+#include <cstddef>
 
-	public:
-	//constructor and destructor
-		Array(void) { _array = new T() };
-		Array(unsigned int n){ _array = new T(n) };
-		~Array(void) { delete [] _array};
-	//Getter and Setter
+template<typename T>
+class Array
+{
+	T		*arr;
+	size_t	len;
 
-	private:
-		T	*_array;
+public:
+	Array()
+	{
+		arr = new T[0];
+		len = 0;
+	}
 
+	Array(unsigned int n)
+	{
+		arr = new T[n];
+		len = n;
+	}
+
+	Array(Array const &rhs)
+	{
+		arr = new T[rhs.len];
+		size_t	i;
+		while (i < rhs.len)
+		{
+			arr[i] = rhs.arr[i];
+			i++;
+		}
+		len = rhs.len;
+	}
+
+	Array	&operator=(Array const &rhs)
+	{
+		delete[] arr;
+		arr = new T[rhs.len];
+
+		size_t	i;
+		while (i < rhs.len)
+		{
+			arr[i] = rhs.arr[i];
+			i++;
+		}
+		len = rhs.len;
+		return (*this);
+	}
+
+	T		&operator[](size_t i)
+	{
+		if (i >= len)
+			throw std::exception();
+		else
+			return arr[i];
+	}
+
+	size_t	size()
+	{
+		return (len);
+	}
 };
 
 #endif
--- day07/ex02/main.cpp
+++ day07/ex02/main.cpp
@@ -3,16 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tgrangeo <tgrangeo@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: thomasgrangeon <thomasgrangeon@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/19 15:06:29 by tgrangeo          #+#    #+#             */
-/*   Updated: 2021/07/19 15:07:20 by tgrangeo         ###   ########lyon.fr   */
+/*   Updated: 2021/07/20 11:09:10 by thomasgrang      ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Array.hpp"
+#include <iostream>
 
-int main(void){
-	Array a;
-	return 0;
+int		main()
+{
+	Array<int> arr(15);
+
+	size_t	i = 0;
+	while (i < arr.size())
+	{
+		std::cout << arr[i] << " ";
+		i++;
+	}
+	return (0);
 }
